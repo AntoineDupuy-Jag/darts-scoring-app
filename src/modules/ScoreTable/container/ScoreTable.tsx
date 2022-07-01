ad43fe226b44f7059985586c7bd2f3d9529bf474
Antoine <akabsn78@gmail.com>
2022-07-01 18:45:18 +0200

    ScoreTable logic score beginning

--- src/modules/ScoreTable/container/ScoreTable.tsx
+++ src/modules/ScoreTable/container/ScoreTable.tsx
@@ -6,17 +6,19 @@ import { Separator } from '../../common-ui/Separator/Separator';
 import { DartIcon1 } from '../../common-ui/Svg';
 import { RulesReminder } from '../components/RulesReminder/RulesReminder';
 import { TargetContainer } from '../components/TargetContainer/TargetContainer';
+import { Multiplier } from '../components/Multiplier/Multiplier';
+import { ScoreButtons } from '../components/ScoreButtons/ScoreButtons';
 import { selectedRulesType, teamsType } from '../../../utils/types';
 import classNames from 'classnames';
 
 import styles from './styles.module.scss';
-import { Multiplier } from '../components/Multiplier/Multiplier';
-import { ScoreButtons } from '../components/ScoreButtons/ScoreButtons';
 
 type ScoreTableProps = {
 	teams: teamsType;
 	selectedRules: selectedRulesType;
 };
+
+type dartArrayType = number[];
 
 export const ScoreTable = ({ teams, selectedRules }: ScoreTableProps) => {
 	const [teamsWithScore, setTeamsWithScore] = useState({} as teamsType);
@@ -25,31 +27,54 @@ export const ScoreTable = ({ teams, selectedRules }: ScoreTableProps) => {
 	// Pas forcément utile...
 	const [dartValue, setDartValue] = useState(0);
 	const [multiplier, setMultiplier] = useState(1);
+	const [dartScore, setDartScore] = useState(0);
 	// OK : à réutiliser
-	const [playerTurn, setPlayerTurn] = useState({ team: 0, player: 0 });
+	const [dartArray, setDartArray] = useState([] as dartArrayType);
+	const [teamTurn, setTeamTurn] = useState({ team: 0 });
 
-	// Get values of buttons keyboard
-	const handleDartValue = (e: any) => {
-		setDartValue(e.target.value);
-		// créer variable tableau vide pour les 3 fléchettes
-		// à la 4ème valeur réinitialiser le tableau à zéro
-		// décrementer le score à partir de la value dart
-		// utiliser le multiplier
-		// setTeamTurn + 1 au bout des 3 fléchettes
-	};
-	console.log('dartValue ->', dartValue);
-
-	// Get value of multiplier radio inputs
-	const handleMultiplier = (e: any) => {
-		setMultiplier(e.target.value);
-	};
-	console.log('multiplierValue ->', multiplier);
-
+	// useEffect() à garder ?
 	useEffect(() => {
 		const newTeams = teams.map((t) => ({ ...t, score: +selectedRules.scoreToGoal }));
 		setTeamsWithScore(newTeams);
 		setIsLoading(false);
 	}, []);
+
+	const handleDartValue = (e: any) => {
+		// ==== V A R I A B L E S ====
+		// Valeur de la fléchette (0 à 50) ->
+		const dartValue = e.target.value;
+		// Copie du tableau vide pour stocker les 3 fléchettes ->
+		const newDartArray: number[] = [...dartArray];
+
+		// ==== L O G I Q U E ====
+		// Décrémenter le score de l'équipe selon dartValue et mulitplier ->
+		const newArray = [...teamsWithScore];
+		newArray[teamTurn.team] = {
+			...newArray[teamTurn.team],
+			score: newArray[teamTurn.team].score - dartValue * multiplier,
+		};
+		setTeamsWithScore(newArray);
+		// Envoyer chaque fléchette dans le tableau dartArray ->
+		newDartArray.push(dartValue);
+		setDartArray(newDartArray);
+		console.log('dartArray ->', dartArray);
+		// Au bout de 3 entrées dans le tableau, on passe à l'équipe suivante ->
+		if (dartArray.length === 3) {
+			setDartArray([]);
+			// Passer à l'équipe suivante en réinitialisant l'objet teamTurn ->
+			setTeamTurn({ team: teamTurn.team + 1 });
+		}
+
+		// à la 4ème valeur réinitialiser le tableau à zéro
+		// décrementer le score à partir de la value dart
+		// utiliser le multiplier
+		// setTeamTurn + 1 au bout des 3 fléchettes
+	};
+
+	const handleMultiplier = (e: any) => {
+		setMultiplier(e.target.value);
+	};
+	// console.log('multiplierValue ->', multiplier);
 
 	// // OK : réutiliser dans handleDartValue()
 	// const newArray = [...teamsWithScore];
@@ -79,8 +104,7 @@ export const ScoreTable = ({ teams, selectedRules }: ScoreTableProps) => {
 									{team.players.map((player, indexPlayer) => (
 										<div
 											className={classNames(styles.playerName, {
-												[styles.playerNameOn]:
-													indexTeam === playerTurn.team && indexPlayer === playerTurn.player,
+												[styles.playerNameOn]: indexTeam === teamTurn.team,
 											})}
 											key={indexPlayer}
 										>
