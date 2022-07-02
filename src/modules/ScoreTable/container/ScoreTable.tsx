4a71348328a7482f325e9b04be26714c2ece52ee
Antoine <akabsn78@gmail.com>
2022-07-02 02:37:16 +0200

    score logic on ScoreTable component

--- src/modules/ScoreTable/container/ScoreTable.tsx
+++ src/modules/ScoreTable/container/ScoreTable.tsx
@@ -18,19 +18,12 @@ type ScoreTableProps = {
 	selectedRules: selectedRulesType;
 };
 
-type dartArrayType = number[];
-
 export const ScoreTable = ({ teams, selectedRules }: ScoreTableProps) => {
 	const [teamsWithScore, setTeamsWithScore] = useState({} as teamsType);
 	const [isLoading, setIsLoading] = useState(true);
-
-	// Pas forcément utile...
-	const [dartValue, setDartValue] = useState(0);
 	const [multiplier, setMultiplier] = useState(1);
-	const [dartScore, setDartScore] = useState(0);
-	// OK : à réutiliser
-	const [dartArray, setDartArray] = useState([] as dartArrayType);
-	const [teamTurn, setTeamTurn] = useState({ team: 0 });
+	const [dartArray, setDartArray] = useState([] as number[]);
+	const [turnToPlay, setTurnToPlay] = useState({ team: 0, player: 0 });
 
 	// useEffect() à garder ?
 	useEffect(() => {
@@ -39,72 +32,60 @@ export const ScoreTable = ({ teams, selectedRules }: ScoreTableProps) => {
 		setIsLoading(false);
 	}, []);
 
-	const handleDartValue = (e: any) => {
-		// ==== V A R I A B L E S ====
-		// Valeur de la fléchette (0 à 50) ->
-		const dartValue = e.target.value;
-		// Copie du tableau vide pour stocker les 3 fléchettes ->
-		const newDartArray: number[] = [...dartArray];
-
-		// ==== L O G I Q U E ====
-		// Décrémenter le score de l'équipe selon dartValue et mulitplier ->
-		const newArray = [...teamsWithScore];
-		newArray[teamTurn.team] = {
-			...newArray[teamTurn.team],
-			score: newArray[teamTurn.team].score - dartValue * multiplier,
-		};
-		setTeamsWithScore(newArray);
-		// Envoyer chaque fléchette dans le tableau dartArray ->
-		newDartArray.push(dartValue);
-		setDartArray(newDartArray);
-		console.log('dartArray ->', dartArray);
-		// Au bout de 3 entrées dans le tableau, on passe à l'équipe suivante ->
-		if (dartArray.length === 3) {
-			setDartArray([]);
-			// Passer à l'équipe suivante en réinitialisant l'objet teamTurn ->
-			setTeamTurn({ team: teamTurn.team + 1 });
-		}
-
-		// à la 4ème valeur réinitialiser le tableau à zéro
-		// décrementer le score à partir de la value dart
-		// utiliser le multiplier
-		// setTeamTurn + 1 au bout des 3 fléchettes
-	};
-
 	const handleMultiplier = (e: any) => {
 		setMultiplier(e.target.value);
 	};
-	// console.log('multiplierValue ->', multiplier);
 
-	// // OK : réutiliser dans handleDartValue()
-	// const newArray = [...teamsWithScore];
-	// newArray[teamIndex] = {
-	// ...newArray[teamIndex],
-	// score: newArray[teamIndex].score - dartValue * multiplier,
-	// };
-	// setTeamsWithScore(newArray);
-	// setPlayerTurn(. . .)
+	// ONCLICK FUNCTION ON SCORE'S BUTTONS
+	const handleDartValue = (e: any) => {
+		const dartValue = e.target.value;
+		const newDartArray = [...dartArray];
+		newDartArray.push(dartValue);
+		setDartArray(newDartArray);
+		if (newDartArray.length >= 3) {
+			setTurnToPlay({
+				team: turnToPlay.team + 1 !== teamsWithScore.length ? turnToPlay.team + 1 : turnToPlay.team - 1,
+				player: 0,
+			});
+			setDartArray([]);
+		}
+		updateScore(turnToPlay.team, dartValue);
+	};
+
+	// UPDATE SCORE FUNCTION
+	const updateScore = (indexTeam: number, dartValue: number) => {
+		const newArray = [...teamsWithScore];
+		newArray[indexTeam] = {
+			...newArray[indexTeam],
+			score: newArray[indexTeam].score - dartValue * multiplier,
+		};
+		setTeamsWithScore(newArray);
+	};
+
+	// --- TESTS ---
+	console.log('dartArray.length', dartArray.length);
+	console.log('dartArray ->', dartArray);
 
 	return (
 		<Container>
 			<Chip name={'Scores'} />
 			<RulesReminder selectedRules={selectedRules} />
 			<Separator />
-
-			{/* TEAMS CONTAINER */}
+			{/* TEAM CONTAINER */}
 			{!isLoading && (
 				<div className={styles.teamsContainer}>
 					{teamsWithScore.map((team, indexTeam) => (
 						// TEAM BOXES
 						<div className={styles.teamBox} key={indexTeam}>
-							{/* NAMES AND DARTS */}
+							{/* PLAYER'S NAMES */}
 							<div className={styles.teamNameLabel}>{'Joueurs'}</div>
 							<div className={styles.teamNames}>
 								<div>
 									{team.players.map((player, indexPlayer) => (
 										<div
 											className={classNames(styles.playerName, {
-												[styles.playerNameOn]: indexTeam === teamTurn.team,
+												[styles.playerNameOn]:
+													indexTeam === turnToPlay.team && indexPlayer === turnToPlay.player,
 											})}
 											key={indexPlayer}
 										>
@@ -112,11 +93,14 @@ export const ScoreTable = ({ teams, selectedRules }: ScoreTableProps) => {
 										</div>
 									))}
 								</div>
-								<div className={styles.dartsContainer}>
-									<DartIcon1 />
-									<DartIcon1 color="white" />
-									<DartIcon1 color="white" />
-								</div>
+								{/* PLAYER'S DARTS */}
+								{indexTeam === turnToPlay.team && (
+									<div className={styles.dartsContainer}>
+										{[...Array(3).keys()].map((index) => (
+											<DartIcon1 color={dartArray.length >= index + 1 ? '' : 'white'} key={index} />
+										))}
+									</div>
+								)}
 							</div>
 							{/* TEAM SCORE */}
 							<div className={styles.teamScore} key={indexTeam}>
@@ -127,7 +111,6 @@ export const ScoreTable = ({ teams, selectedRules }: ScoreTableProps) => {
 					))}
 				</div>
 			)}
-
 			<Multiplier onChange={handleMultiplier} />
 			<ScoreButtons onClick={handleDartValue} />
 		</Container>
