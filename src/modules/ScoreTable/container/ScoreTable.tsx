d535d8ac3e1cec09944ab2d7e1c8c6d2900d0e47
Antoine <akabsn78@gmail.com>
2022-07-08 00:11:43 +0200

    double rules in ScoreTable component

--- src/modules/ScoreTable/container/ScoreTable.tsx
+++ src/modules/ScoreTable/container/ScoreTable.tsx
@@ -21,12 +21,12 @@ type ScoreTableProps = {
 export const ScoreTable = ({ teams, selectedRules }: ScoreTableProps) => {
 	const [teamsWithScore, setTeamsWithScore] = useState({} as teamsType);
 	const [isLoading, setIsLoading] = useState(true);
-	const [multiplier, setMultiplier] = useState(1);
+	const [multiplier, setMultiplier] = useState(1 as number);
 	const [dartArray, setDartArray] = useState([] as number[]);
 	const [roundsNumber, setRoundsNumber] = useState(0);
 	const [turnToPlay, setTurnToPlay] = useState({ team: 0, player: 0 });
 
-	// useEffect() à garder ?
+	// useEffect() : à garder ?
 	useEffect(() => {
 		const newTeams = teams.map((t) => ({ ...t, score: +selectedRules.scoreToGoal }));
 		setTeamsWithScore(newTeams);
@@ -39,8 +39,8 @@ export const ScoreTable = ({ teams, selectedRules }: ScoreTableProps) => {
 
 	// ONCLICK FUNCTION ON SCORE'S BUTTONS
 	const handleDartValue = (e: any) => {
-		const dartValue = e.target.value * multiplier;
 		const newDartArray = [...dartArray];
+		const dartValue = e.target.value * multiplier;
 		newDartArray.push(dartValue);
 		setDartArray(newDartArray);
 		// To know if all the teams have played
@@ -62,10 +62,10 @@ export const ScoreTable = ({ teams, selectedRules }: ScoreTableProps) => {
 			});
 			setDartArray([]);
 		}
-		updateScore(turnToPlay.team, dartValue);
+		updateScoreWithDoubleRules(turnToPlay.team, dartValue);
 	};
 
-	// UPDATE SCORE FUNCTION
+	// UPDATE SCORE SIMPLE
 	const updateScore = (indexTeam: number, dartValue: number) => {
 		const newArray = [...teamsWithScore];
 		newArray[indexTeam] = {
@@ -76,6 +76,62 @@ export const ScoreTable = ({ teams, selectedRules }: ScoreTableProps) => {
 					: newArray[indexTeam].score - dartValue,
 		};
 		setTeamsWithScore(newArray);
+	};
+
+	// CANCEL DARTS IF THE SCORE EXCEEDS THE LIMIT
+	// const updateScore = (indexTeam: number, dartValue: number) => {
+	// 	const newArray = [...teamsWithScore];
+	// 	console.log('dartArray POUR UPDATE ->', dartArray);
+	// 	// console.log('dartValue ->', dartValue);
+	// 	if (newArray[indexTeam].score - dartValue < 0) {
+	// 		// console.log('dartArray.indexOf(dartValue) ->', dartArray.indexOf(dartValue));
+	// 		// Third dart
+	// 		if (dartArray.indexOf(dartValue) === -1) {
+	// 			newArray[indexTeam] = {
+	// 				...newArray[indexTeam],
+	// 				score: newArray[indexTeam].score + dartArray[0] + dartArray[1],
+	// 			};
+	// 		}
+	// 		// Second dart
+	// 		if (dartArray.indexOf(dartValue) === 1) {
+	// 			newArray[indexTeam] = {
+	// 				...newArray[indexTeam],
+	// 				score: newArray[indexTeam].score + dartArray[0],
+	// 			};
+	// 		}
+	// 		// First dart
+	// 		if (dartArray.indexOf(dartValue) === 0) {
+	// 			newArray[indexTeam] = {
+	// 				...newArray[indexTeam],
+	// 				score: newArray[indexTeam].score,
+	// 			};
+	// 		}
+	// 	} else {
+	// 		newArray[indexTeam] = {
+	// 			...newArray[indexTeam],
+	// 			score: newArray[indexTeam].score - dartValue,
+	// 		};
+	// 	}
+	// 	setTeamsWithScore(newArray);
+	// };
+
+	// UPDATE SCORE ACCORDING TO DOUBLE RULES
+	const updateScoreWithDoubleRules = (indexTeam: number, dartValue: number) => {
+		const beginning = teamsWithScore[indexTeam].score === +selectedRules.scoreToGoal;
+		const ending = teamsWithScore[indexTeam].score - dartValue <= 0;
+		const doubleIn = selectedRules.doublesOrNot === 'Double pour entrer';
+		const doubleOut = selectedRules.doublesOrNot === 'Double pour sortir';
+		const doubleInAndOut = selectedRules.doublesOrNot === 'Double pour entrer et sortir';
+		if (beginning) {
+			if ((doubleIn || doubleInAndOut) && multiplier == 2) updateScore(indexTeam, dartValue);
+			if (!doubleIn && !doubleInAndOut) updateScore(indexTeam, dartValue);
+		}
+		if (ending) {
+			if ((doubleOut || doubleInAndOut) && multiplier == 2) updateScore(indexTeam, dartValue);
+			if (!doubleOut && !doubleInAndOut) updateScore(indexTeam, dartValue);
+		}
+		if (!beginning && !ending) updateScore(indexTeam, dartValue);
+		if (!doubleIn && !doubleOut && !doubleInAndOut) updateScore(indexTeam, dartValue);
 	};
 
 	// CANCEL DART FUNCTION
@@ -91,10 +147,6 @@ export const ScoreTable = ({ teams, selectedRules }: ScoreTableProps) => {
 		newDartArray.pop();
 		setDartArray(newDartArray);
 	};
-
-	// --- TESTS ---
-	// console.log('dartArray.length', dartArray.length);
-	// console.log('dartArray ->', dartArray);
 
 	return (
 		<Container>
