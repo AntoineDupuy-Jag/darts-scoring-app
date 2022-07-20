bf89ea66ae6d6f8d1b53f3edf818fdb27f512f32
Antoine <akabsn78@gmail.com>
2022-07-20 02:54:53 +0200

    work on style of Podium component

--- src/modules/Results/components/Podium/Podium.tsx
+++ src/modules/Results/components/Podium/Podium.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import styles from './styles.module.scss';
 
 type PodiumProps = {
@@ -8,22 +9,25 @@ type PodiumProps = {
 export const Podium = ({ label, value }: PodiumProps) => {
 	return (
 		<div className={styles.container}>
-			<div className={styles.secondPlace}>
+			<div className={styles.column1}>
 				<div className={styles.label}>{'Marine'}</div>
-				<div className={styles.step}>
-					<div>{'51,25'}</div>
+				<div className={classNames(styles.place, styles.secondPlace)}>
+					<div className={styles.value}>{'52'}</div>
+					<div className={classNames(styles.position, styles.position2)}>{'2'}</div>
 				</div>
 			</div>
-			<div className={styles.firstPlace}>
+			<div className={styles.column2}>
 				<div className={styles.label}>{'Antoine'}</div>
-				<div className={styles.step}>
-					<div>{'60'}</div>
+				<div className={classNames(styles.place, styles.firstPlace)}>
+					<div className={styles.value}>{'62'}</div>
+					<div className={classNames(styles.position, styles.position1)}>{'1'}</div>
 				</div>
 			</div>
-			<div className={styles.thirdPlace}>
+			<div className={styles.column3}>
 				<div className={styles.label}>{'Alice'}</div>
-				<div className={styles.step}>
-					<div>{'44,2'}</div>
+				<div className={classNames(styles.place, styles.thirdPlace)}>
+					<div className={styles.value}>{'42.5'}</div>
+					<div className={classNames(styles.position, styles.position3)}>{'3'}</div>
 				</div>
 			</div>
 		</div>
--- src/modules/Results/container/Results.tsx
+++ src/modules/Results/container/Results.tsx
@@ -1,6 +1,7 @@
 import { StatisticsType, teamType } from '../../../utils/types';
 import { Chip } from '../../common-ui/Chip/Chip';
 import { Container } from '../../common-ui/Container/Container';
+import { Podium } from '../components/Podium/Podium';
 
 import styles from './styles.module.scss';
 
@@ -17,13 +18,15 @@ export const Results = ({ statistics, winningTeam }: ResultsProps) => {
 				{`⭐ ${winningTeam.name} remporte la partie ! 🏆`}
 			</div>
 			<div className={styles.stats}>
-				{statistics.map((stat) => (
+				<div className={styles.leftSide}>React-select and others stats...</div>
+				<Podium />
+				{/* {statistics.map((stat) => (
 					<div>
 						<div>{stat.playerName}</div>
 						<div>{stat.multiples.doubles}</div>
 						<div>{stat.multiples.triples}</div>
 					</div>
-				))}
+				))} */}
 			</div>
 		</Container>
 	);
